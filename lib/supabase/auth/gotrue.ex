2495e46c5529b65439d64a804a1a0ce3a6f1a6ca
Patrick Muehlbauer <patrick@opentrash.org>
2021-05-05 00:29:36 +0200

    adapt gotrue functions to latest internal api changes

--- lib/supabase/auth/gotrue.ex
+++ lib/supabase/auth/gotrue.ex
@@ -3,18 +3,12 @@ defmodule Supabase.Auth.GoTrue do
 
   @spec send_magic_link_email(Connection.t(), String.t()) :: any
   def send_magic_link_email(%Connection{} = conn, email) do
-    case Connection.post(conn, "/auth/v1/magiclink", {:json, %{email: email}}) do
-      %Finch.Response{status: 200} -> {:ok, "success"}
-      %Finch.Response{body: body} -> {:error, body}
-    end
+    Connection.post(conn, "/auth/v1/magiclink", {:json, %{email: email}})
   end
 
   @spec sign_up(Connection.t(), String.t(), String.t()) :: {:error, map()} | {:ok, map()}
   def sign_up(%Connection{} = conn, email, password) do
-    case Connection.post(conn, "/auth/v1/signup", {:json, %{email: email, password: password}}) do
-      %Finch.Response{body: body, status: 200} -> {:ok, body}
-      %Finch.Response{body: body} -> {:error, body}
-    end
+    Connection.post(conn, "/auth/v1/signup", {:json, %{email: email, password: password}})
   end
 
   @spec sign_in(Connection.t(), String.t()) :: {:ok, String.t()} | {:error, map()}
@@ -24,36 +18,27 @@ defmodule Supabase.Auth.GoTrue do
 
   @spec sign_in(Connection.t(), String.t(), String.t()) :: {:ok, String.t()} | {:error, map()}
   def sign_in(%Connection{} = conn, email, password) do
-    case Connection.post(
-           conn,
-           "/auth/v1/token?grant_type=password",
-           {:json, %{email: email, password: password}}
-         ) do
-      %Finch.Response{body: body, status: 200} -> {:ok, body}
-      %Finch.Response{body: body} -> {:error, body}
-    end
+    Connection.post(
+      conn,
+      "/auth/v1/token?grant_type=password",
+      {:json, %{email: email, password: password}}
+    )
   end
 
   @spec refresh_access_token(Connection.t(), String.t()) :: {:ok, map()} | {:error, map()}
   def refresh_access_token(%Connection{} = conn, refresh_token) do
-    case Connection.post(
-           conn,
-           "/auth/v1/token?grant_type=refresh_token",
-           {:json, %{refresh_token: refresh_token}}
-         ) do
-      %Finch.Response{body: body, status: 200} -> {:ok, body}
-      %Finch.Response{body: body} -> {:error, body}
-    end
+    Connection.post(
+      conn,
+      "/auth/v1/token?grant_type=refresh_token",
+      {:json, %{refresh_token: refresh_token}}
+    )
   end
 
   def user(%Connection{} = conn, access_token) do
-    case Connection.get(
-           conn,
-           "/auth/v1/user",
-           headers: [{"Authorization", "Bearer #{access_token}"}]
-         ) do
-      %Finch.Response{body: body, status: 200} -> {:ok, body}
-      %Finch.Response{body: body} -> {:error, body}
-    end
+    Connection.get(
+      conn,
+      "/auth/v1/user",
+      headers: [{"Authorization", "Bearer #{access_token}"}]
+    )
   end
 end
