6b132413b58ac9e2a8ed78a0d235f7813bb0b9bd
Dylan C. Kendal <dylankendal@gmail.com>
2023-11-06 16:19:50 -0500

    Add config back for dev and production for supabase urls
    This shouldn't be in the config as a library module.

--- config/runtime.exs
+++ config/runtime.exs
@@ -5,4 +5,9 @@ case Mix.env() do
     config :supabase,
       base_url: System.fetch_env!("SUPABASE_TEST_URL"),
       api_key: System.fetch_env!("SUPABASE_TEST_KEY")
+
+  _ ->
+    config :supabase,
+      base_url: System.fetch_env!("SUPABASE_URL"),
+      api_key: System.fetch_env!("SUPABASE_KEY")
 end
